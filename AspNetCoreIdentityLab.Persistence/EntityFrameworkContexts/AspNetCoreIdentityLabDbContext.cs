d25994f2a8d9b6da54b116f264d4d679613a9ca8
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-08-29 08:30:42 -0300

    Add and configure AspNetCoreIdentityLabDbContext

---
+++ AspNetCoreIdentityLab.Persistence/EntityFrameworkContexts/AspNetCoreIdentityLabDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreIdentityLab.Persistence.EntityFrameworkContexts
+{
+    public class AspNetCoreIdentityLabDbContext : DbContext
+    {
+        private const string ConnectionString = @"Server=192.168.1.14,22331;Database=AspNetCoreIdentityLab;User ID=sa;Password=sqlserver.252707;
+                                                  Encrypt=False;Trusted_Connection=False;Connection Timeout=3000;";
+
+        public static readonly ILoggerFactory LoggerFactoryToConsole = LoggerFactory.Create(builder => builder.AddConsole());
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) return;
+
+            optionsBuilder.UseSqlServer(ConnectionString);
+            optionsBuilder.UseLoggerFactory(LoggerFactoryToConsole);
+        }
+    }
+}
