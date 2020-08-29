4a6e5550f5a9570eb09b808fb9b3458c049dc64c
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-08-29 07:43:04 -0300

    Create startup solution
    Provides only initial project and folder structure. The solution not executed yet. The next commits will be prepare the code for basic execution.

---
+++ AspNetCoreIdentityLab.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreIdentityLab.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
