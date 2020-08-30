55adf1e104b724e2003b741965e442e8c0fc6ab1
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-08-30 07:37:55 -0300

    Add register pages scaffold to project

---
+++ AspNetCoreIdentityLab.Application/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AspNetCoreIdentityLab.Persistence.DataTransferObjects;
+using AspNetCoreIdentityLab.Persistence.EntityFrameworkContexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AspNetCoreIdentityLab.Application.Areas.Identity.IdentityHostingStartup))]
+namespace AspNetCoreIdentityLab.Application.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
