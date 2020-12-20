1edb0a8c3957dbf34d792fa28a504521ac60a000
Stenio Nobres Batista <stenio.nobres@gmail.com>
2020-12-20 11:03:28 -0300

    Encrypts 2FA authenticator token
    To improves data security in database

--- AspNetCoreIdentityLab.Application/Custom/UserManager.cs
+++ AspNetCoreIdentityLab.Application/Custom/UserManager.cs
@@ -1,16 +1,36 @@
-﻿using AspNetCoreIdentityLab.Persistence.DataTransferObjects;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
 using System.Collections.Generic;
+using AspNetCoreIdentityLab.Application.Tools;
+using AspNetCoreIdentityLab.Persistence.DataTransferObjects;
 
 namespace AspNetCoreIdentityLab.Application.Custom
 {
     public class UserManager : UserManager<User>
     {
         private readonly IConfiguration _configuration;
+
+        private bool EncryptionEnabled
+        {
+            get
+            {
+                bool.TryParse(_configuration["TwoFactorAuthentication:EncryptionEnabled"], out bool encryptionEnabled);
+
+                return encryptionEnabled;
+            }
+        }
+
+        private string EncryptionKey
+        {
+            get
+            {
+                return _configuration["TwoFactorAuthentication:EncryptionKey"];
+            }
+        }
 
         public UserManager(IUserStore<User> store, IOptions<IdentityOptions> optionsAccessor, 
                            IPasswordHasher<User> passwordHasher, IEnumerable<IUserValidator<User>> userValidators, 
@@ -19,6 +39,30 @@ namespace AspNetCoreIdentityLab.Application.Custom
                            : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
         {
             _configuration = configuration;
+        }
+
+        public override string GenerateNewAuthenticatorKey()
+        {
+            var originalAuthenticatorKey = base.GenerateNewAuthenticatorKey();
+
+            var encryptedKey = EncryptionEnabled
+                               ? new AesEncryptor(EncryptionKey).Encrypt(originalAuthenticatorKey)
+                               : originalAuthenticatorKey;
+
+            return encryptedKey;
+        }
+
+        public override async Task<string> GetAuthenticatorKeyAsync(User user)
+        {
+            var databaseKey = await base.GetAuthenticatorKeyAsync(user);
+
+            if (databaseKey == null) return null;
+            
+            var originalAuthenticatorKey = EncryptionEnabled
+                                           ? new AesEncryptor(EncryptionKey).Decrypt(databaseKey)
+                                           : databaseKey;
+
+            return originalAuthenticatorKey;
         }
     }
 }
