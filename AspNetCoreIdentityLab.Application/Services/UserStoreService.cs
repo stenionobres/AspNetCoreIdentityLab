c91be7c227ca67b253c64b8770f5cdbf88584e8e
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-01-09 17:27:36 -0300

    Complements basics support to Dapper persistence
    It's important to say that only create account and login routines was tested. Another services need to be tested and/or implemented.

--- AspNetCoreIdentityLab.Application/Services/UserStoreService.cs
+++ AspNetCoreIdentityLab.Application/Services/UserStoreService.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using AspNetCoreIdentityLab.Persistence.IdentityStores;
@@ -7,7 +6,7 @@ using AspNetCoreIdentityLab.Persistence.DataTransferObjects;
 
 namespace AspNetCoreIdentityLab.Application.Services
 {
-    public class UserStoreService : IUserStore<User>, IUserPasswordStore<User>
+    public class UserStoreService : IUserStore<User>, IUserPasswordStore<User>, IUserPhoneNumberStore<User>, IUserEmailStore<User>
     {
         private readonly UserStore _userStore;
 
@@ -53,22 +52,22 @@ namespace AspNetCoreIdentityLab.Application.Services
 
         public Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return _userStore.SetNormalizedUserNameAsync(user, normalizedName, cancellationToken);
         }
 
         public Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return _userStore.SetUserNameAsync(user, userName, cancellationToken);
         }
 
         public Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return _userStore.UpdateAsync(user, cancellationToken);
         }
 
         public Task SetPasswordHashAsync(User user, string passwordHash, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return _userStore.SetPasswordHashAsync(user, passwordHash, cancellationToken);
         }
 
         public Task<string> GetPasswordHashAsync(User user, CancellationToken cancellationToken)
@@ -78,12 +77,68 @@ namespace AspNetCoreIdentityLab.Application.Services
 
         public Task<bool> HasPasswordAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return _userStore.HasPasswordAsync(user, cancellationToken);
+        }
+
+        public Task SetPhoneNumberAsync(User user, string phoneNumber, CancellationToken cancellationToken)
+        {
+            return _userStore.SetPhoneNumberAsync(user, phoneNumber, cancellationToken);
+        }
+
+        public Task<string> GetPhoneNumberAsync(User user, CancellationToken cancellationToken)
+        {
+            return _userStore.GetPhoneNumberAsync(user, cancellationToken);
+        }
+
+        public Task<bool> GetPhoneNumberConfirmedAsync(User user, CancellationToken cancellationToken)
+        {
+            return _userStore.GetPhoneNumberConfirmedAsync(user, cancellationToken);
+        }
+
+        public Task SetPhoneNumberConfirmedAsync(User user, bool confirmed, CancellationToken cancellationToken)
+        {
+            return _userStore.SetPhoneNumberConfirmedAsync(user, confirmed, cancellationToken);
+        }
+
+        public Task SetEmailAsync(User user, string email, CancellationToken cancellationToken)
+        {
+            return _userStore.SetEmailAsync(user, email, cancellationToken);
+        }
+
+        public Task<string> GetEmailAsync(User user, CancellationToken cancellationToken)
+        {
+            return _userStore.GetEmailAsync(user, cancellationToken);
+        }
+
+        public Task<bool> GetEmailConfirmedAsync(User user, CancellationToken cancellationToken)
+        {
+            return _userStore.GetEmailConfirmedAsync(user, cancellationToken);
+        }
+
+        public Task SetEmailConfirmedAsync(User user, bool confirmed, CancellationToken cancellationToken)
+        {
+            return _userStore.SetEmailConfirmedAsync(user, confirmed, cancellationToken);
+        }
+
+        public Task<User> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken)
+        {
+            return _userStore.FindByEmailAsync(normalizedEmail, cancellationToken);
+        }
+
+        public Task<string> GetNormalizedEmailAsync(User user, CancellationToken cancellationToken)
+        {
+            return _userStore.GetNormalizedEmailAsync(user, cancellationToken);
+        }
+
+        public Task SetNormalizedEmailAsync(User user, string normalizedEmail, CancellationToken cancellationToken)
+        {
+            return _userStore.SetNormalizedEmailAsync(user, normalizedEmail, cancellationToken);
         }
 
         public void Dispose()
         {
 
         }
+
     }
 }
--- AspNetCoreIdentityLab.Persistence/IdentityStores/UserStore.cs
+++ AspNetCoreIdentityLab.Persistence/IdentityStores/UserStore.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Dapper;
+﻿using Dapper;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
@@ -13,9 +12,52 @@ namespace AspNetCoreIdentityLab.Persistence.IdentityStores
         private const string ConnectionString = @"Server=192.168.1.5,22331;Database=AspNetCoreIdentityLab;User ID=sa;Password=sqlserver.252707;
                                                   Encrypt=False;Trusted_Connection=False;Connection Timeout=3000;";
         
-        public Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken)
+        public async Task<IdentityResult> CreateAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+
+                var query = $@"INSERT INTO [AspNetUsers] 
+                                ([UserName], 
+                                 [NormalizedUserName], 
+                                 [Email],
+                                 [NormalizedEmail], 
+                                 [EmailConfirmed], 
+                                 [PasswordHash], 
+                                 [SecurityStamp], 
+                                 [ConcurrencyStamp], 
+                                 [PhoneNumber], 
+                                 [PhoneNumberConfirmed], 
+                                 [TwoFactorEnabled],
+                                 [LockoutEnd],
+                                 [LockoutEnabled],
+                                 [AccessFailedCount],
+                                 [Occupation])
+                                VALUES (
+                                    @{nameof(User.UserName)}, 
+                                    @{nameof(User.NormalizedUserName)}, 
+                                    @{nameof(User.Email)},
+                                    @{nameof(User.NormalizedEmail)}, 
+                                    @{nameof(User.EmailConfirmed)}, 
+                                    @{nameof(User.PasswordHash)},
+                                    @{nameof(User.SecurityStamp)},
+                                    @{nameof(User.ConcurrencyStamp)},
+                                    @{nameof(User.PhoneNumber)}, 
+                                    @{nameof(User.PhoneNumberConfirmed)}, 
+                                    @{nameof(User.TwoFactorEnabled)},
+                                    @{nameof(User.LockoutEnd)},
+                                    @{nameof(User.LockoutEnabled)},
+                                    @{nameof(User.AccessFailedCount)},
+                                    @{nameof(User.Occupation)});
+                                SELECT CAST(SCOPE_IDENTITY() as int)";
+
+                user.Id = await connection.QuerySingleAsync<int>(query, user);
+            }
+
+            return IdentityResult.Success;
         }
 
         public async Task<IdentityResult> DeleteAsync(User user, CancellationToken cancellationToken)
@@ -31,9 +73,16 @@ namespace AspNetCoreIdentityLab.Persistence.IdentityStores
             return IdentityResult.Success;
         }
 
-        public Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        public async Task<User> FindByIdAsync(string userId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+                return await connection.QuerySingleOrDefaultAsync<User>($@"SELECT * FROM [AspNetUsers]
+                             WHERE [Id] = @{nameof(userId)}", new { userId });
+            }
         }
 
         public async Task<User> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
@@ -70,7 +119,7 @@ namespace AspNetCoreIdentityLab.Persistence.IdentityStores
 
         public Task<bool> HasPasswordAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(user.PasswordHash != null);
         }
 
         public Task SetNormalizedUserNameAsync(User user, string normalizedName, CancellationToken cancellationToken)
@@ -81,7 +130,8 @@ namespace AspNetCoreIdentityLab.Persistence.IdentityStores
 
         public Task SetPasswordHashAsync(User user, string passwordHash, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            user.PasswordHash = passwordHash;
+            return Task.FromResult(0);
         }
 
         public Task SetUserNameAsync(User user, string userName, CancellationToken cancellationToken)
@@ -90,14 +140,101 @@ namespace AspNetCoreIdentityLab.Persistence.IdentityStores
             return Task.FromResult(0);
         }
 
-        public Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
+        public async Task<IdentityResult> UpdateAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+                await connection.ExecuteAsync($@"UPDATE [AspNetUsers] SET
+                    [UserName] = @{nameof(user.UserName)},
+                    [NormalizedUserName] = @{nameof(user.NormalizedUserName)},
+                    [Email] = @{nameof(user.Email)},
+                    [NormalizedEmail] = @{nameof(user.NormalizedEmail)},
+                    [EmailConfirmed] = @{nameof(user.EmailConfirmed)},
+                    [PasswordHash] = @{nameof(user.PasswordHash)},
+                    [SecurityStamp] = @{nameof(user.SecurityStamp)},
+                    [ConcurrencyStamp] = @{nameof(user.ConcurrencyStamp)},
+                    [PhoneNumber] = @{nameof(user.PhoneNumber)},
+                    [PhoneNumberConfirmed] = @{nameof(user.PhoneNumberConfirmed)},
+                    [TwoFactorEnabled] = @{nameof(user.TwoFactorEnabled)},
+                    [LockoutEnd] = @{nameof(user.LockoutEnd)},
+                    [LockoutEnabled] = @{nameof(user.LockoutEnabled)},
+                    [AccessFailedCount] = @{nameof(user.AccessFailedCount)},
+                    [Occupation] = @{nameof(user.Occupation)}
+                    WHERE [Id] = @{nameof(user.Id)}", user);
+            }
+
+            return IdentityResult.Success;
         }
 
-        public void Dispose()
+        public Task SetPhoneNumberAsync(User user, string phoneNumber, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            user.PhoneNumber = phoneNumber;
+            return Task.FromResult(0);
         }
+
+        public Task<string> GetPhoneNumberAsync(User user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PhoneNumber);
+        }
+
+        public Task<bool> GetPhoneNumberConfirmedAsync(User user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.PhoneNumberConfirmed);
+        }
+
+        public Task SetPhoneNumberConfirmedAsync(User user, bool confirmed, CancellationToken cancellationToken)
+        {
+            user.PhoneNumberConfirmed = confirmed;
+            return Task.FromResult(0);
+        }
+
+        public Task SetEmailAsync(User user, string email, CancellationToken cancellationToken)
+        {
+            user.Email = email;
+            return Task.FromResult(0);
+        }
+
+        public Task<string> GetEmailAsync(User user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.Email);
+        }
+
+        public Task<bool> GetEmailConfirmedAsync(User user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.EmailConfirmed);
+        }
+
+        public Task SetEmailConfirmedAsync(User user, bool confirmed, CancellationToken cancellationToken)
+        {
+            user.EmailConfirmed = confirmed;
+            return Task.FromResult(0);
+        }
+
+        public async Task<User> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                await connection.OpenAsync(cancellationToken);
+                return await connection.QuerySingleOrDefaultAsync<User>($@"SELECT * FROM [AspNetUsers]
+                             WHERE [NormalizedEmail] = @{nameof(normalizedEmail)}", new { normalizedEmail });
+            }
+        }
+
+        public Task<string> GetNormalizedEmailAsync(User user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.NormalizedEmail);
+        }
+
+        public Task SetNormalizedEmailAsync(User user, string normalizedEmail, CancellationToken cancellationToken)
+        {
+            user.NormalizedEmail = normalizedEmail;
+            return Task.FromResult(0);
+        }
+        
     }
 }
