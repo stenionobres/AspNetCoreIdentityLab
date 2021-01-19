d3f0ed7014c22665cdbdf7e2fa500fdd858d2c02
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-01-18 21:14:26 -0300

    Adds User SignUp Api

---
+++ AspNetCoreIdentityLab.Api/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreIdentityLab.Api.Model;
+using AspNetCoreIdentityLab.Persistence.DataTransferObjects;
+
+namespace AspNetCoreIdentityLab.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserModel userModel)
+        {
+            var user = new User() { UserName = userModel.Email, Email = userModel.Email, Occupation = userModel.Occupation };
+
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+
+            if (result.Succeeded)
+            {
+                return Created(string.Empty, new { user.Id, user.Email, user.Occupation });
+            }
+
+            var errorDetail = result.Errors.First().Description;
+
+            return Problem(detail: errorDetail, instance: null, statusCode: 500);
+        }
+    }
+}
---
+++ AspNetCoreIdentityLab.Api/Model/UserModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AspNetCoreIdentityLab.Api.Model
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public string Occupation { get; set; }
+        public string Password { get; set; }
+    }
+}
