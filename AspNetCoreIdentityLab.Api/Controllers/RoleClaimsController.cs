bcf3516d435276a98751f606c558fee323039f2c
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-01-29 18:13:26 -0300

    Adds RoleClaims api service

---
+++ AspNetCoreIdentityLab.Api/Controllers/RoleClaimsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreIdentityLab.Api.Model;
+
+namespace AspNetCoreIdentityLab.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleClaimsController : ControllerBase
+    {
+        private readonly RoleManager<IdentityRole<int>> _roleManager;
+
+        public RoleClaimsController(RoleManager<IdentityRole<int>> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Save(RoleClaimModel roleClaimModel)
+        {
+            var claim = new Claim("Policy", roleClaimModel.PolicyId.ToString());
+            var role = await _roleManager.FindByIdAsync(roleClaimModel.RoleId.ToString());
+
+            if (role == null)
+            {
+                return Problem(detail: "There is no role with this id", instance: null, statusCode: 500);
+            }
+
+            var identityResult = await _roleManager.AddClaimAsync(role, claim);
+
+            if (identityResult.Succeeded)
+            {
+                return Ok("Claim added to Role");
+            }
+
+            var errorDetail = identityResult.Errors.First().Description;
+
+            return Problem(detail: errorDetail, instance: null, statusCode: 500);
+        }
+    }
+}
---
+++ AspNetCoreIdentityLab.Api/Model/RoleClaimModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AspNetCoreIdentityLab.Api.Model
+{
+    public class RoleClaimModel
+    {
+        public int RoleId { get; set; }
+        public int PolicyId { get; set; }
+    }
+}
