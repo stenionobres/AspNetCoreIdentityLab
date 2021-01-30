dd5b62d210e800b6e3673c0e265f30ab84db323f
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-01-30 09:05:02 -0300

    Adds UserClaims api service

---
+++ AspNetCoreIdentityLab.Api/Controllers/UserClaimsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreIdentityLab.Api.Model;
+using AspNetCoreIdentityLab.Persistence.DataTransferObjects;
+
+namespace AspNetCoreIdentityLab.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserClaimsController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserClaimsController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Save(UserClaimModel userClaimModel)
+        {
+            var claim = new Claim("Policy", userClaimModel.PolicyId.ToString());
+            var user = await _userManager.FindByIdAsync(userClaimModel.UserId.ToString());
+
+            if (user == null)
+            {
+                return Problem(detail: "There is no user with this id", instance: null, statusCode: 500);
+            }
+
+            var identityResult = await _userManager.AddClaimAsync(user, claim);
+
+            if (identityResult.Succeeded)
+            {
+                return Ok("Claim added to User");
+            }
+
+            var errorDetail = identityResult.Errors.First().Description;
+
+            return Problem(detail: errorDetail, instance: null, statusCode: 500);
+        }
+    }
+}
---
+++ AspNetCoreIdentityLab.Api/Model/UserClaimModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AspNetCoreIdentityLab.Api.Model
+{
+    public class UserClaimModel
+    {
+        public int UserId { get; set; }
+        public int PolicyId { get; set; }
+    }
+}
