043c3da5f346a20973b0b3d200715a8e96bc9adc
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-02-04 16:56:13 -0300

    Adds Employee controller to exemplify dynamic authorization

---
+++ AspNetCoreIdentityLab.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreIdentityLab.Api.DynamicAuthorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityLab.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HasPermission(Permissions.CanCreateEmployee)]
+        public ActionResult Save()
+        {
+            return Ok();
+        }
+
+        [HasPermission(Permissions.CanReadEmployee)]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
